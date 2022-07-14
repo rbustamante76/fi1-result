101f8d9597171073f1d0ef225833ba925c82a251
prismatic <42003049+astudillom@users.noreply.github.com>
2022-07-14 03:49:15 -0400

    problemas con nombres
    se cambia a app.component como componente  padre, se modifica su selector para no tener alcance de nombre con el selector de otro proyecto, se le añade ViewEncapsulation para encapsular y generar el ShadowDOM  y a su html se le agrega el componente f1-result.
    Se modifica el archivo app.module para definir AppComponent como componente a inyectar y definir, este se define bajo otro nombre de selector, el cual es el mismo utilizado en el index de este proyecto.
    Se limpian espacios en blancos apreciados en el proyecto.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-race',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'fi1-result';
 }
